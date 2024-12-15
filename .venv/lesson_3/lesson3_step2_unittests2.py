2ed9fb00d403c36f98d78b17a2cdfe4f4af28251
Yauheni-Aliakseyenka <alekseenka.evv@gmail.com>
2024-12-15 15:57:48 +0300

    15.12 unittest refactor

--- .venv/lesson_3/lesson3_step2_unittests2.py
+++ .venv/lesson_3/lesson3_step2_unittests2.py
@@ -5,30 +5,23 @@ import time
 
 
 class TestAbs(unittest.TestCase):
-    '''def set_up(self):
-        self.browser = webdriver.Chrome
-        self.browser.implicitly_wait(2)'''
+    def set_up(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(1)
 
-    '''def fill_form(self, link):
+    def fill_form(self, link):
         self.browser.get(link)
         f_name = self.browser.find_element(By.CSS_SELECTOR, '.first_block .first')
         f_name.send_keys('John')
         l_name = self.browser.find_element(By.CSS_SELECTOR, '.first_block .second')
         l_name.send_keys('Doe')
         email = self.browser.find_element(By.CSS_SELECTOR, '.first_block .third')
-        email.send_keys('test@test.com')'''
+        email.send_keys('test@test.com')
 
     def test_1(self):
-        self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(1)
+        self.set_up()
         link1 = "http://suninjuly.github.io/registration1.html"
-        self.browser.get(link1)
-        f_name = self.browser.find_element(By.CSS_SELECTOR, '.first_block .first')
-        f_name.send_keys('John')
-        l_name = self.browser.find_element(By.CSS_SELECTOR, '.first_block .second')
-        l_name.send_keys('Doe')
-        email = self.browser.find_element(By.CSS_SELECTOR, '.first_block .third')
-        email.send_keys('test@test.com')
+        self.fill_form(link1)
         button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
         wel_text_elt = self.browser.find_element(By.TAG_NAME, "h1")
@@ -36,23 +29,15 @@ class TestAbs(unittest.TestCase):
         self.assertEqual("Congratulations! You have successfully registered!", text, 'Success test_1')
 
     def test_2(self):
-        self.browser = webdriver.Chrome()
-        self.browser.implicitly_wait(1)
+        self.set_up()
         link2 = "http://suninjuly.github.io/registration2.html"
-        self.browser.get(link2)
-        f_name = self.browser.find_element(By.CSS_SELECTOR, '.first_block .first')
-        f_name.send_keys('John')
-        l_name = self.browser.find_element(By.CSS_SELECTOR, '.first_block .second')
-        l_name.send_keys('Doe')
-        email = self.browser.find_element(By.CSS_SELECTOR, '.first_block .third')
-        email.send_keys('test@test.com')
+        self.fill_form(link2)
         button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
         wel_text_elt = self.browser.find_element(By.TAG_NAME, "h1")
         text = wel_text_elt.text
         self.assertEqual("Congratulations! You have successfully registered!", text, 'Success test_2')
 
+
 if __name__ == "__main__":
     unittest.main()
-
-
